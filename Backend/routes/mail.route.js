97a87623e2cbff69b5839cd712b75b68e79728e9
Yaman Gahlout <yamangahlout123@gmail.com>
2026-01-17 21:12:43 +0530

    Backend removed

--- Backend/routes/mail.route.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { sendEmail } = require("../controllers/email.controller");
-
-const router = express.Router();
-router.post("/send-email", sendEmail);
-module.exports = router;
